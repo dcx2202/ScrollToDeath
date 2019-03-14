581a7fe44ca973549fa376e3349a146300a4417a
Diogo Cruz <31130828+dcx2202@users.noreply.github.com>
2019-03-14 19:38:29 +0000

    Add files via upload

--- ScrollToDeath/background.js
+++ ScrollToDeath/background.js
@@ -3,12 +3,25 @@ chrome.tabs.onUpdated.addListener(function(info) {
     
     var tracked_urls;
     
-    chrome.storage.sync.get(['key'], function(result) {
+    chrome.storage.sync.get(['tracked_urls'], function(result) {
         tracked_urls = result.key;
       });
 
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         console.log(tabs[0].url);
+
+        /*var tracked = false;
+
+        for(url of tracked_urls)
+        {
+            if(tabs[0].url.startsWith(url))
+            {
+                tracked = true;
+                chrome.storage.sync.get(['timer'], function(result) {
+                    tracked_urls = result.key;
+                  });
+            }
+        }*/
       });
 
     //var timer = document.getElementsById("timer");
